c04b93681204bba820538d39b615835f9dd770d2
nnorian <kushnirenkoecaterina@gmail.com>
2026-02-18 18:37:35 +0200

    feat: intruducent 4 interfaces

---
+++ src/domain/interfaces.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from domain.entities import Event, Rule, Alert
+
+class ILogParser(ABC):
+    #contract for anything that loads detection rules from a source 
+
+    @bstractmethod
+    def load(self, source: str) -> List[Rule]:
+        # loads rules and returns a list of rule objects 
+
+class IDetector(ABC):
+# contract for detection engine
+# events coresponding to certain rules make the alert
+    @abstractmethod
+    def detect(self, events: List[Event], rule: List[Rule]) -> List[Alert]:
+
+class IReporter(ABC):
+    #outputs detection rules
+    @abstractmethod
+    def report(self, alerts: List[Alert], total_events: int) -> None:
